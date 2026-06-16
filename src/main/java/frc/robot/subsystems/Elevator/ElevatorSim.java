e2fc5ab46944b94e17ea767041bbf21d379d035d
rayArjun2025 <rayankula.arjun@gmail.com>
2026-06-16 01:54:10 -0700

    Working on ElevatorSim, need to add setters for the fields

--- src/main/java/frc/robot/subsystems/Elevator/ElevatorSim.java
+++ src/main/java/frc/robot/subsystems/Elevator/ElevatorSim.java
@@ -1,5 +1,36 @@
 package frc.robot.subsystems.Elevator;
 
-public class ElevatorSim {
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N2;
+import edu.wpi.first.math.system.LinearSystem;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.wpilibj.simulation.DCMotorSim;
+
+public class ElevatorSim implements ElevatorIO{
+    private double position = 0;
+    private double velocity = 0;
+    private double motorVoltage = 0;
+
+    private LinearSystem<N2, N1, N2> elevatorSystem;
+    private final DCMotorSim elevatorMotorSim;
+    
+    public ElevatorSim(){
+        elevatorSystem = LinearSystemId.createDCMotorSystem(1.0, 1.0);
+        elevatorMotorSim = new DCMotorSim(elevatorSystem, DCMotor.getKrakenX60Foc(1), 1.0, 1.0);
+    }
+
+    @Override
+    public void updateInputs(ElevatorIOInputs inputs) {
+        
+        velocity += motorVoltage * 0.1; 
+        position += velocity * 0.02; 
+
+        inputs.elevatorPositionMeters = position;
+        inputs.elevatorVelocityMetersPerSec = velocity;
+        inputs.atTop = position >= ElevatorConstants.ELEVATOR_MAX_HEIGHT;
+        inputs.atBottom = position <= ElevatorConstants.ELEVATOR_MIN_HEIGHT;
+    }
+
     
 }
