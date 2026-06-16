57d90d4aa3b0a048bda66fa80d8055d0b91f7746
rayArjun2025 <rayankula.arjun@gmail.com>
2026-06-16 02:11:04 -0700

    Added more sim functionality and pid controllers to calculate velocity, need to calculate andf update position

--- src/main/java/frc/robot/subsystems/Elevator/ElevatorSim.java
+++ src/main/java/frc/robot/subsystems/Elevator/ElevatorSim.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems.Elevator;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N2;
 import edu.wpi.first.math.system.LinearSystem;
@@ -8,9 +9,11 @@ import edu.wpi.first.math.system.plant.LinearSystemId;
 import edu.wpi.first.wpilibj.simulation.DCMotorSim;
 
 public class ElevatorSim implements ElevatorIO{
-    private double position = 0;
-    private double velocity = 0;
+    private double elevatorPosition = 0;
+    private double elevatorVelocity = 0;
+    private double elevatorCurrent = 0;
     private double motorVoltage = 0;
+    private PIDController pid;
 
     private LinearSystem<N2, N1, N2> elevatorSystem;
     private final DCMotorSim elevatorMotorSim;
@@ -18,19 +21,34 @@ public class ElevatorSim implements ElevatorIO{
     public ElevatorSim(){
         elevatorSystem = LinearSystemId.createDCMotorSystem(1.0, 1.0);
         elevatorMotorSim = new DCMotorSim(elevatorSystem, DCMotor.getKrakenX60Foc(1), 1.0, 1.0);
+        pid = new PIDController(1.0, 0.0, 0.0);
     }
 
     @Override
     public void updateInputs(ElevatorIOInputs inputs) {
-        
-        velocity += motorVoltage * 0.1; 
-        position += velocity * 0.02; 
-
-        inputs.elevatorPositionMeters = position;
-        inputs.elevatorVelocityMetersPerSec = velocity;
-        inputs.atTop = position >= ElevatorConstants.ELEVATOR_MAX_HEIGHT;
-        inputs.atBottom = position <= ElevatorConstants.ELEVATOR_MIN_HEIGHT;
+        inputs.elevatorPositionMeters = elevatorPosition;
+        inputs.elevatorVelocityMetersPerSec = elevatorVelocity;
+        inputs.elevatorMotorVolts = motorVoltage;
+        inputs.elevatorMotorCurrent = elevatorCurrent;
+        inputs.atTop = elevatorPosition >= ElevatorConstants.ELEVATOR_MAX_HEIGHT;
+        inputs.atBottom = elevatorPosition <= ElevatorConstants.ELEVATOR_MIN_HEIGHT;
     }
+
+    public void setMotorVoltage(double voltage) {
+        elevatorMotorSim.setInputVoltage(voltage);
+    }
+
+    public void setMoveVelocity(double velocity_mps) {
+        motorVoltage = pid.calculate(elevatorVelocity, velocity_mps);
+        elevatorMotorSim.setInputVoltage(motorVoltage);
+    }
+
+    public void stopMoving() {
+        setMoveVelocity(0);
+        setMotorVoltage(0);
+    }
+
+
 
     
 }
